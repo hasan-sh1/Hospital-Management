64d387b01200ce480951cddd8bd3bba58881e4ef
RanimRamadan <ranimramadan456@gmail.com>
2025-03-14 01:39:19 +0300

    Break down the components in the system

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,90 @@
+// import React from "react";
+// import Link from "next/link";
+// import { FaFacebookF, FaGoogle, FaTwitter, FaSkype, FaLinkedin, FaSearch } from "react-icons/fa";
+// import { FiMenu } from "react-icons/fi";
+
+// const Navbar: React.FC = () => {
+//   return (
+//     <header className="main-header">
+//       {/* Header Top */}
+//       <div className="header-top">
+//         <div className="auto-container clearfix">
+//           <div className="top-left clearfix">
+//             <ul className="list">
+//               <li><span className="icon fas fa-envelope"></span> 2130 Fulton Street San Diego CA 94117-1080 USA</li>
+//               <li><span className="icon fas fa-phone"></span> <a href="tel:+555–123–2323">555–123–2323</a></li>
+//             </ul>
+//           </div>
+//           <div className="top-right clearfix">
+//             <ul className="social-icons">
+//               <li><a href="#"><FaFacebookF /></a></li>
+//               <li><a href="#"><FaGoogle /></a></li>
+//               <li><a href="#"><FaTwitter /></a></li>
+//               <li><a href="#"><FaSkype /></a></li>
+//               <li><a href="#"><FaLinkedin /></a></li>
+//             </ul>
+//           </div>
+//         </div>
+//       </div>
+//       {/* End Header Top */}
+
+//       {/* Header Upper */}
+//       <div className="header-upper">
+//         <div className="inner-container">
+//           <div className="auto-container clearfix">
+//             {/* Logo */}
+//             <div className="logo-outer">
+//               <div className="logo">
+//                 <Link href="/">
+//                   <a><img src="/images/logo.png" alt="Medi-tech Logo" /></a>
+//                 </Link>
+//               </div>
+//             </div>
+
+//             {/* Nav Box */}
+//             <div className="nav-outer clearfix">
+//               <div className="mobile-nav-toggler">
+//                 <span className="icon"><FiMenu /></span>
+//               </div>
+//               <nav className="main-menu navbar-expand-md navbar-light">
+//                 <div className="navbar-header">
+//                   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+//                     <span className="icon flaticon-menu"></span>
+//                   </button>
+//                 </div>
+
+//                 <div className="collapse navbar-collapse clearfix" id="navbarSupportedContent">
+//                   <ul className="navigation clearfix">
+//                     <li className="current dropdown">
+//                       <a href="#">Home</a>
+//                       <ul>
+//                         <li><a href="/home">Home page 01</a></li>
+//                       </ul>
+//                     </li>
+//                     <li className="dropdown">
+//                       <a href="#">About us</a>
+//                       <ul>
+//                         <li><a href="/about">About Us</a></li>
+//                         <li><a href="/team">Our Team</a></li>
+//                       </ul>
+//                     </li>
+//                     <li className="dropdown">
+//                       <a href="#">Doctors</a>
+//                       <ul>
+//                         <li><a href="/doctors">Doctors</a></li>
+//                       </ul>
+//                     </li>
+//                     <li><a href="/contact">Contact</a></li>
+//                   </ul>
+//                 </div>
+//               </nav>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//       {/* End Header Upper */}
+//     </header>
+//   );
+// };
+
+// export default Navbar;
